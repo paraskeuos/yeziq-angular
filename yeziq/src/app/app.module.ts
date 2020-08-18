9c9f76f180fecc273d8596fa5848fa2b08ce901c
zephyros <milan.radisic@gmail.com>
2020-08-18 14:36:57 +0200

    Added initial project, components and adequate flexing for the home page

---
+++ yeziq/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './home-page/navbar/navbar.component';
+import { MainComponent } from './home-page/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    NavbarComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
