601b14ea341d9e25d7a6d4f06b2564b4dbff6980
zephyros <milan.radisic@gmail.com>
2020-08-23 17:41:34 +0200

    Registration form added

--- yeziq/src/app/login-page/register-form/dist/register-form.component.js
+++ yeziq/src/app/login-page/register-form/dist/register-form.component.js
@@ -8,10 +8,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.RegisterFormComponent = void 0;
 var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
 var RegisterFormComponent = /** @class */ (function () {
-    function RegisterFormComponent() {
+    function RegisterFormComponent(formBuilder) {
+        this.formBuilder = formBuilder;
         this.emitFormSwitch = new core_1.EventEmitter();
+        this.registerForm = this.formBuilder.group({
+            username: ['', [forms_1.Validators.required, forms_1.Validators.minLength(4), forms_1.Validators.maxLength(25)]],
+            password: ['', [forms_1.Validators.required, forms_1.Validators.minLength(6), forms_1.Validators.maxLength(25)]],
+            confirmPassword: ['', [forms_1.Validators.required, this.confirmPasswordValidator()]],
+            email: ['', [forms_1.Validators.required, forms_1.Validators.email]],
+            //targetLang: [this.srcLang === 'en' ? 'de' : 'en']
+            targetLang: 'de'
+        });
     }
+    RegisterFormComponent.prototype.confirmPasswordValidator = function () {
+        var _this = this;
+        return function (control) {
+            if (!_this.registerForm)
+                return null;
+            var passwordConfirmed = control.value === '' ? false : control.value === _this.registerForm.get('password').value;
+            return passwordConfirmed ? null : { unconfirmedPassword: true };
+        };
+    };
+    RegisterFormComponent.prototype.submitRegisterInfo = function () {
+    };
     RegisterFormComponent.prototype.switchForms = function () {
         this.emitFormSwitch.emit(true);
     };
--- yeziq/src/app/login-page/register-form/register-form.component.ts
+++ yeziq/src/app/login-page/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -10,7 +11,31 @@ export class RegisterFormComponent implements OnInit {
   @Output('formSwitch')
   public emitFormSwitch: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor() { }
+  public registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(25)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(25)]],
+      confirmPassword: ['', [Validators.required, this.confirmPasswordValidator()]],
+      email: ['', [Validators.required, Validators.email]],
+      //targetLang: [this.srcLang === 'en' ? 'de' : 'en']
+      targetLang: 'de'
+    });
+  }
+
+  private confirmPasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors => {
+      if (!this.registerForm)
+        return null;
+      const passwordConfirmed = control.value === '' ? false : control.value === this.registerForm.get('password').value;
+      return passwordConfirmed ? null : { unconfirmedPassword: true };
+    }
+  }
+
+  submitRegisterInfo() {
+    
+  }
 
   switchForms(): void {
     this.emitFormSwitch.emit(true);
