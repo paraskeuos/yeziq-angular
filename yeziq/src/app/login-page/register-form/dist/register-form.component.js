8ad4124f6f8d0e12f186e45ef11e2bb097b1ad5c
zephyros <milan.radisic@gmail.com>
2020-08-24 16:03:36 +0200

    Implemented registration

--- yeziq/src/app/login-page/register-form/dist/register-form.component.js
+++ yeziq/src/app/login-page/register-form/dist/register-form.component.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -10,15 +21,19 @@ exports.RegisterFormComponent = void 0;
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
 var RegisterFormComponent = /** @class */ (function () {
-    function RegisterFormComponent(formBuilder) {
+    function RegisterFormComponent(formBuilder, userService, router) {
         this.formBuilder = formBuilder;
+        this.userService = userService;
+        this.router = router;
         this.emitFormSwitch = new core_1.EventEmitter();
+        this.showErrors = false;
+        this.serverMsg = '';
+        this.activeSubs = [];
         this.registerForm = this.formBuilder.group({
             username: ['', [forms_1.Validators.required, forms_1.Validators.minLength(4), forms_1.Validators.maxLength(25)]],
             password: ['', [forms_1.Validators.required, forms_1.Validators.minLength(6), forms_1.Validators.maxLength(25)]],
             confirmPassword: ['', [forms_1.Validators.required, this.confirmPasswordValidator()]],
             email: ['', [forms_1.Validators.required, forms_1.Validators.email]],
-            //targetLang: [this.srcLang === 'en' ? 'de' : 'en']
             targetLang: 'de'
         });
     }
@@ -32,11 +47,65 @@ var RegisterFormComponent = /** @class */ (function () {
         };
     };
     RegisterFormComponent.prototype.submitRegisterInfo = function () {
+        var _this = this;
+        this.serverMsg = '';
+        if (!this.registerForm.valid) {
+            this.showErrors = true;
+            return;
+        }
+        this.showErrors = false;
+        var data = __assign(__assign({}, this.registerForm.value), { srcLang: 'en' });
+        var registerSub = this.userService.createUser(data).subscribe(function (user) {
+            if (user) {
+                var loginSub = _this.userService.login({ username: data.username, password: data.password }).subscribe(function (obj) {
+                    if (user)
+                        _this.router.navigate(['/courses']);
+                });
+                _this.activeSubs.push(loginSub);
+            }
+            else {
+                _this.serverMsg = 'User already exists.';
+            }
+        });
+        this.activeSubs.push(registerSub);
+        this.registerForm.reset();
+        this.registerForm.get('targetLang').setValue('de');
     };
     RegisterFormComponent.prototype.switchForms = function () {
         this.emitFormSwitch.emit(true);
     };
+    Object.defineProperty(RegisterFormComponent.prototype, "username", {
+        get: function () {
+            return this.registerForm.get('username');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(RegisterFormComponent.prototype, "password", {
+        get: function () {
+            return this.registerForm.get('password');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(RegisterFormComponent.prototype, "confirmPassword", {
+        get: function () {
+            return this.registerForm.get('confirmPassword');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(RegisterFormComponent.prototype, "email", {
+        get: function () {
+            return this.registerForm.get('email');
+        },
+        enumerable: false,
+        configurable: true
+    });
     RegisterFormComponent.prototype.ngOnInit = function () {
+    };
+    RegisterFormComponent.prototype.ngOnDestroy = function () {
+        this.activeSubs.forEach(function (sub) { return sub.unsubscribe(); });
     };
     __decorate([
         core_1.Output('formSwitch')
--- yeziq/src/app/login-page/register-form/register-form.component.ts
+++ yeziq/src/app/login-page/register-form/register-form.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
+import { UserServiceService } from 'src/app/services/user-service.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/models/user.model';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent implements OnInit {
+export class RegisterFormComponent implements OnInit, OnDestroy {
 
   @Output('formSwitch')
   public emitFormSwitch: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   public registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  public showErrors = false;
+  public serverMsg = '';
+
+  public activeSubs: Subscription[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserServiceService,
+              private router: Router) { 
+
     this.registerForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(25)]],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(25)]],
       confirmPassword: ['', [Validators.required, this.confirmPasswordValidator()]],
       email: ['', [Validators.required, Validators.email]],
-      //targetLang: [this.srcLang === 'en' ? 'de' : 'en']
       targetLang: 'de'
     });
   }
@@ -34,14 +45,63 @@ export class RegisterFormComponent implements OnInit {
   }
 
   submitRegisterInfo() {
-    
+    this.serverMsg = '';
+
+    if (!this.registerForm.valid) {
+      this.showErrors = true;
+
+      return;
+    }
+
+    this.showErrors = false;
+
+    const data = {
+      ...this.registerForm.value,
+      srcLang: 'en'
+    };
+    const registerSub = this.userService.createUser(data).subscribe((user: User) => {
+      if (user) {
+
+        const loginSub = this.userService.login({ username: data.username, password: data.password }).subscribe((obj: any) => {
+          if (user)
+            this.router.navigate(['/courses']);
+        });
+
+        this.activeSubs.push(loginSub);
+
+      } else {
+        this.serverMsg = 'User already exists.';
+      }
+    });
+    this.activeSubs.push(registerSub);
+    this.registerForm.reset();
+    this.registerForm.get('targetLang').setValue('de');
   }
 
   switchForms(): void {
     this.emitFormSwitch.emit(true);
   }
 
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.activeSubs.forEach(sub => sub.unsubscribe());
+  }
 }
